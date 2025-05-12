e61b18fb7e2de280a89b4ff3a7ede1dfa917371a
HONGDALEE03 <2720997234@qq.com>
2025-05-12 14:38:41 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -25,26 +25,23 @@ class ResidualBlock(nn.Module):
         residual = x
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += residual  # 残差连接
-        return F.relu(out)
+        out += residual 
+        out = F.relu(out)
+        return out
 
-class AE_model(nn.Module):
+class VAE(nn.Module):
     def __init__(self, in_channels=3, base_channels=32):
         super().__init__()
-        
-        # 初始编码层
         self.initial_conv = nn.Sequential(
             nn.Conv2d(in_channels, base_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(base_channels),
             nn.ReLU()
         )
-        
-        # 16个残差块
+   
         self.residual_blocks = nn.Sequential(
-            *[ResidualBlock(base_channels) for _ in range(16)]
+            *[ResidualBlock(base_channels) for _ in range(8)]
         )
-        
-        # 最终编码层（带残差连接）
+ 
         self.final_encoder = nn.Sequential(
             nn.Conv2d(base_channels, base_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(base_channels)
@@ -54,50 +51,52 @@ class AE_model(nn.Module):
             nn.Flatten()
         )
         
-        self.fc = nn.Linear(32 * 64 * 64, 2*base_channels)
-        self.decoder_fc = nn.Linear(2*base_channels, 32 * 64 * 64)
-        
+        self.fc_mu = nn.Linear(base_channels* 64 * 64, 4 * base_channels)
+        self.fc_log_var = nn.Sequential(
+            nn.Linear(base_channels * 64 * 64, 4 * base_channels),
+            nn.Softplus() 
+        )
+          
+        self.decoder_fc = nn.Linear(4 * base_channels, base_channels * 64 * 64)
         # 解码器
         self.decoder = nn.Sequential(
-            # 第一层解码模块
             nn.Conv2d(base_channels, base_channels*4, kernel_size=3, padding=1),
             PixelShuffleUpsample(base_channels*4, base_channels),
             nn.BatchNorm2d(base_channels),
             nn.ReLU(),
             
-            # 第二层解码模块
             nn.Conv2d(base_channels, base_channels*4, kernel_size=3, padding=1),
             PixelShuffleUpsample(base_channels*4, base_channels//2),
             nn.BatchNorm2d(base_channels//2),
             nn.ReLU(),
             
-            # 最终输出层
             nn.Conv2d(base_channels//2, 3, kernel_size=3, padding=1),
             nn.Sigmoid()
         )
         
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5*log_var)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+     
     def forward(self, x):
-        # 初始编码
         initial_out = self.initial_conv(x)
-        # 残差块处理
         residual_out = self.residual_blocks(initial_out)
-        # 最终编码（带残差）
         encoded = self.final_encoder(residual_out) + initial_out
         z = self.flatten(encoded)
-        z = self.fc(z)
+
+        mu = self.fc_mu(z)
+        log_var = self.fc_log_var(z)
+        z = self.reparameterize(mu, log_var)
         h = self.decoder_fc(z)
         h = h.view(-1, 32, 64, 64)
-        # 解码
+    
         decoded = self.decoder(h)
         
-        return decoded, z
-
-
+        return decoded, mu, log_var
+    
 if __name__=='__main__':
-  
-  data = torch.zeros([11, 3, 64, 64])
-  model = AE_model()
-  output, _ = model(data)
-  print(output.shape)
-
-
+    data = torch.zeros([11, 3, 64, 64])
+    model = VAE()
+    output, _, _ = model(data)
+    print(output.shape)
--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import torch
 from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset 
 import torchvision.utils as vutils
-from torch.utils.data import DataLoader, Dataset
-import numpy as np
 from matplotlib import pyplot as plt
-from model import AE_model
-from PIL import Image
 import os
+import numpy as np
+from PIL import Image
+from model import VAE
 
-os.chdir("D:\\code\\AE")
+os.chdir("D:\\code\\VAE")
 class StressDataset(Dataset):
     def __init__(self, stress_dir, transform=None):
         self.stress_dir = stress_dir
         self.stress_files = [os.path.join(stress_dir, f) for f in os.listdir(stress_dir) if f.endswith('.png')]
-        transform = transform 
+        self.transform = transform
 
     def __len__(self):
         return len(self.stress_files)
@@ -22,50 +22,70 @@ class StressDataset(Dataset):
         img_path = self.stress_files[idx]
         stress = Image.open(img_path).convert('RGB')
         stress1 = np.array(stress).astype(np.float32).transpose(2, 0, 1)/255.0
-
-        if transform:
-            stress = transform(stress)
-        # stress = np.array(stress)
-        # plt.imshow(stress)
-        # plt.show()
-        return stress, stress1
+        
+        if self.transform:
+            stress = self.transform(stress)
+        return stress,stress1
 
 transform = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor(),
 ])
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = AE_model().to(device)
-model.load_state_dict(torch.load('ae_model_state_dict.pth'))
-model.eval()
-
-testdataset = StressDataset(
+traindataset = StressDataset(
     stress_dir=r"D:\\cosegGuitar\\256x256\\stress",
+    transform=transform
 )
-dataloader = DataLoader(testdataset, batch_size=1, shuffle=False, num_workers=0)
+dataloader = DataLoader(traindataset, batch_size=8, shuffle=None, num_workers=0)
 
-plt.close('all')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = VAE().to(device)
+model.load_state_dict(torch.load('vae_model_state_dict.pth'))
+model.eval()
+with torch.no_grad(): 
+    for i, (stress, stress1) in enumerate(dataloader):
+        stress = stress[0:1].to(device)
+        recon_stress, _ , _ = model(stress)
+        stress1 = stress1[0:1].to(device)
+        temp1 = stress1.numpy().squeeze().transpose(1,2,0)
+        temp2 = recon_stress.numpy().squeeze().transpose(1,2,0)
+        break
 
-if __name__=='__main__':
+mus = []
+log_vars = []
+with torch.no_grad():
+    for batch_idx, (stress, stress1) in enumerate(dataloader):
+        stress = stress.to(device)
+        _, mu, log_var = model(stress)
+        mus.append(mu.cpu().numpy())
+        log_vars.append(log_var.cpu().numpy())
 
-    with torch.no_grad():
-        for i, (stress, stress1) in enumerate(dataloader):
-            stress = stress.to(device)
-            recon_stress, _ = model(stress)
-            temp1 = stress1.numpy().squeeze().transpose(1,2,0)
-            temp2 = recon_stress.numpy().squeeze().transpose(1,2,0)
-            plt.figure(1)
-            plt.subplot(1,2,1)
-            plt.imshow(temp1)
-            plt.title("Original")
+mus = np.concatenate(mus, axis=0)
+log_vars = np.concatenate(log_vars, axis=0)
+vars = np.exp(0.5 * log_vars)  
 
-            plt.subplot(1,2,2)
-            plt.imshow(temp2)
-            plt.title("AE")
-            plt.show()
-            break
-    
-    output_dir = "D:\code\GENERATE"
-    os.makedirs(output_dir, exist_ok=True)
-    vutils.save_image(recon_stress, os.path.join(output_dir, "ae_generated_stress.png"), nrow=8, normalize=True)
+plt.figure(1)
+plt.subplot(1, 2, 1)
+plt.imshow(temp1)
+plt.title("Original")
+
+plt.subplot(1, 2, 2)
+plt.imshow(temp2)
+plt.title("VAE")
+plt.show()
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.hist(mus.flatten(), bins=50, color='blue', alpha=0.7)
+plt.title("Distribution of μ (Mean)")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+
+plt.subplot(1, 2, 2)
+plt.hist(vars.flatten(), bins=50, color='red', alpha=0.7)
+plt.title("Distribution of σ (Standard Deviation)")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+
+plt.tight_layout()
+plt.show()
